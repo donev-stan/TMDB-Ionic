14401bf118306d2eee36363072dd05e07bb9d8a1
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-11-24 12:26:35 +0200

    showing trending items

--- src/app/tabs/trending/trending.page.ts
+++ src/app/tabs/trending/trending.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { map } from 'rxjs';
+import {
+  MediaTypeOptions,
+  TimeWindowOptions,
+} from 'src/app/interfaces/db-interfaces';
 import { DbService } from 'src/app/services/db.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-trending',
@@ -7,13 +14,64 @@ import { DbService } from 'src/app/services/db.service';
   styleUrls: ['trending.page.scss'],
 })
 export class TrendingPage implements OnInit {
-  constructor(private dbService: DbService) {}
+  items: any[] = [];
+
+  mediaType: MediaTypeOptions = { media_type: 'all' };
+  timeWindow: TimeWindowOptions = { time_window: 'week' };
+  currentPage: number = 1;
+
+  imageBaseUrl: string = environment.images;
+
+  constructor(
+    private dbService: DbService,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit(): void {
-    this.dbService.getTrending().subscribe({
-      next: (response) => {
-        console.log(response);
-      },
+    this.loadItems();
+  }
+
+  async loadItems(infiniteScrollEvent?: any): Promise<void> {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading...',
+      spinner: 'bubbles',
     });
+
+    await loading.present();
+
+    this.dbService
+      .getTrending(this.mediaType, this.timeWindow, this.currentPage)
+      .pipe(this.attachImagesUrl())
+      .subscribe({
+        next: (response: any) => {
+          this.items.push(...response.results);
+
+          // console.log(response);
+          // console.log(this.items);
+
+          loading.dismiss();
+
+          if (infiniteScrollEvent) {
+            infiniteScrollEvent?.target.complete();
+            infiniteScrollEvent.target.disabled =
+              response.total_pages === this.currentPage;
+          }
+        },
+      });
+  }
+
+  loadMore(event: any) {
+    this.currentPage++;
+    this.loadItems(event);
+  }
+
+  attachImagesUrl() {
+    return map((response: any) => ({
+      ...response,
+      results: response.results.map((result: any) => ({
+        ...result,
+        poster_path: `${this.imageBaseUrl}/w92${result.poster_path}`,
+      })),
+    }));
   }
 }
